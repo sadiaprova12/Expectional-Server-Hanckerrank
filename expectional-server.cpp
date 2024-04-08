a2f6bbdb721eafae77f5bbb929631fc499a3397b
sadiaprova12 <sadiaprova51717@gmail.com>
2024-04-09 02:15:24 +0600

    sloved these solution

--- expectional-server.cpp
+++ expectional-server.cpp
@@ -35,7 +35,15 @@ int main() {
 		cin >> A >> B;
 
 		/* Enter your code here. */
-
+          try {
+      cout << Server::compute(A, B) << endl;
+    } catch (bad_alloc& e) {
+      cout << "Not enough memory" << endl;
+    } catch (exception& e) {
+      cout << "Exception: " << e.what() << endl;
+    } catch (int e) {
+      cout << "Other Exception" << endl;
+    }
 
 	}
 	cout << Server::getLoad() << endl;
